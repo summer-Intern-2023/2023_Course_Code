528e0efccb21cb379c47d1c211028235cbb37362
SZhao112 <ficeiceceream@gmail.com>
2023-06-26 13:17:47 +0800

    feat Create Folders

---
+++ MIT S6.001/Shiwen Zhao/PS1/ps1c.py
@@ -0,0 +1,29 @@
+# User Input
+initial_deposit = float(input("Enter the initial deposit: "))
+
+# Variable initialization
+year = 3
+lowest = 0
+highest = 1
+
+if 159900<=initial_deposit:
+    r = 0.0
+    print("Best savings rate: 0.0")
+    print("Steps in bisection search: 0")
+if (initial_deposit*(1+1/12)**36)<159900:
+    r = None
+    print("Best savings rate: "+str(r))
+    print("Steps in bisection search: 0")
+count = 0
+while True:
+    r = (lowest+highest)/2
+    if (initial_deposit*((1+r/12)**36))<159900:
+        lowest = r
+    if (initial_deposit*((1+r/12)**36))>160100:
+        highest = r
+    if 159900<=(initial_deposit*((1+r/12)**36))<160100:
+        print('Best savings rate: '+str(r))
+        break
+    count+=1
+    
+print("Steps in bisection search: ", count)
---
+++ MIT S6.001/Shiwen Zhao/PS1/x.py
@@ -0,0 +1,1 @@
+print(7 in range(5,10))
