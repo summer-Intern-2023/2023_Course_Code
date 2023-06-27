9ba6196cebaf17379537f63a268b146114c96eae
SZhao112 <ficeiceceream@gmail.com>
2023-06-27 17:09:58 +0800

    style: Change the format

--- ElementsOfAI/Shiwen Zhao/Exercise2-Inter.py
+++ ElementsOfAI/Shiwen Zhao/Exercise2-Inter.py
@@ -5,16 +5,15 @@ def main():
     # nautical miles converted to km
 
     D = [
-            [0,8943,8019,3652,10545],
-            [8943,0,2619,6317,2078],
-            [8019,2619,0,5836,4939],
-            [3652,6317,5836,0,7825],
-            [10545,2078,4939,7825,0]
-        ]
+        [0, 8943, 8019, 3652, 10545],
+        [8943, 0, 2619, 6317, 2078],
+        [8019, 2619, 0, 5836, 4939],
+        [3652, 6317, 5836, 0, 7825],
+        [10545, 2078, 4939, 7825, 0]
+    ]
 
     # https://timeforchange.org/co2-emissions-shipping-goods
     # assume 20g per km per metric ton (of pineapples)
-
 
     co2 = 0.020
     port1 = 0
@@ -29,8 +28,11 @@ def main():
                     if port2 == port5 or port3 == port5 or port4 == port5:
                         continue
                     route = [port1, port2, port3, port4, port5]
-                    distance = D[route[0]][route[1]] + D[route[1]][route[2]] + D[route[2]][route[3]] + D[route[3]][route[4]]
+                    distance = D[route[0]][route[1]] + D[route[1]][route[2]
+                                                                   ] + D[route[2]][route[3]] + D[route[3]][route[4]]
                     emissions = distance * co2
-                    print(' '.join([portnames[i] for i in route]) + " %.1f kg" % emissions)
-    
+                    print(' '.join([portnames[i]
+                          for i in route]) + " %.1f kg" % emissions)
+
+
 main()
--- ElementsOfAI/Shiwen Zhao/Exercise3-Inter.py
+++ ElementsOfAI/Shiwen Zhao/Exercise3-Inter.py
@@ -6,6 +6,7 @@ w = [random.random()/3, random.random()/3, random.random()/3]
 h = [1.+math.sin(1+x/6.)*w[0]+math.sin(-.3+x/9.)*w[1]+math.sin(-.2+x/30.)*w[2] for x in range(100)]
 h[0] = 0.0; h[99] = 0.0
 
+
 def climb(x, h):
     # keep climbing until we've found a summit
     summit = False
@@ -18,8 +19,7 @@ def climb(x, h):
             summit = False    # and keep going
         elif h[x-1] > h[x]:   
             x = x - 1         # left is higher, go there
-            summit = False    # and keep going
-        
+            summit = False    # and keep going        
     return x
 
 
@@ -31,5 +31,6 @@ def main(h):
 
     print("Venla started at %d and got to %d" % (x0, x))
     return x0, x
+
 
 main(h)
