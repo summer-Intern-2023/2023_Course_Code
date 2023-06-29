e1cb81a2b6140f80c373213842a5a73ebb32d9c4
SZhao112 <ficeiceceream@gmail.com>
2023-06-29 10:09:27 +0800

    style: Change code format

--- MIT S6.001/Shiwen Zhao/PS1/ps1a.py
+++ MIT S6.001/Shiwen Zhao/PS1/ps1a.py
@@ -1,6 +1,7 @@
 # User Input
 annual_salary = float(input("Enter your yearly salary: "))
-portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+portion_saved = float(
+    input("Enter the percent of your salary to save, as a decimal: "))
 total_cost = float(input("Enter the cost of your dream home: "))
 
 # Variable initialization
@@ -9,8 +10,8 @@ current_savings = 0
 r = 0.04
 months = 0
 
-while current_savings<total_cost*portion_down_payment:
-    months +=1
-    current_savings+=annual_salary/12*portion_saved+current_savings*r/12
+while current_savings < total_cost*portion_down_payment:
+    months += 1
+    current_savings += annual_salary/12*portion_saved+current_savings*r/12
 
 print('Number of months: '+str(months))
