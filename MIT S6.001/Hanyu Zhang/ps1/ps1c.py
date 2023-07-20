6cb8a226c8a5989aa26419f2e1b4a436f0a6d5f4
HanyuZhang22 <137847685+HanyuZhang22@users.noreply.github.com>
2023-07-20 12:39:38 +0800

    Add files via upload

---
+++ MIT S6.001/Hanyu Zhang/ps1/ps1c.py
@@ -0,0 +1,10 @@
+starting_salary = float(input("Enter the starting salary: "))
+
+total_cost = 1000000
+semi_annual_raise = 0.07
+portion_down_payment = 0.25
+
+r = 0.04
+month = 0
+current_savings = 0.0
+down_payment = total_cost * portion_down_payment
